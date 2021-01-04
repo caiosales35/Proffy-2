3843197d75c7a93e735d07bb97949de09eb88615
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2021-01-03 23:04:48 -0300

    Criando usuario no banco de dados

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import ClassesControler from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
+import UserController from "./controllers/UserController";
 
 const routes = express.Router();
 const classesControler = new ClassesControler();
 const connectionsController = new ConnectionsController();
+const userController = new UserController();
+
+routes.post("/users", userController.create);
 
 routes.get("/classes", classesControler.index);
 routes.post("/classes", classesControler.create);
