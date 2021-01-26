cd425b07cd268c029c1dc5bc30dd9fd4c277a1e9
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2021-01-25 22:38:01 -0300

    Funcao de login web

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 import "./styles.css";
 import logoImg from "../../assets/images/logo.svg";
+import api from "../../services/api";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function signIn(e: FormEvent) {
+    e.preventDefault();
+    api
+      .post("/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => alert("Email ou senha incorretos..."));
+
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="logon">
       <div className="image">
@@ -11,14 +31,28 @@ function Login() {
         <h2 className="titulo">Sua plataforma de estudos online.</h2>
       </div>
       <div className="formContainer">
-        <form className="form">
+        <form className="form" onSubmit={signIn}>
           <h1 className="tituloForm">Proffy - login</h1>
-          <input className="input" type="email" placeholder="Email" />
-          <input className="input" type="text" placeholder="Senha" />
+          <input
+            className="input"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/*
           <div className="connected">
-            <input type="checkbox" value="connected" />
+            <input type="checkbox" value="checked" />
             <label htmlFor="vehicle1"> Continuar conectado</label>
           </div>
+          */}
           <button type="submit">Entrar</button>
           <a>Esqueci minha senha</a>
           <a>Cadastre-se de graça</a>
